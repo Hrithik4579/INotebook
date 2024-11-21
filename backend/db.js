03be265095567ed0d08f40ae76694719c51e8494
Hrithik4579 <hbhagat2003@gmail.com>
2024-11-22 02:27:41 +0530

    deploy

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI='mongodb://127.0.0.1:27017/inotebook';
+const mongoURI='mongodb+srv://hbhagat:hbhagat123@cluster0.zueey.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0';
 const  connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("connected to mongodb");
