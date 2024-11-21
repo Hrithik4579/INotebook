c28b3e13f090dcb73a5adcc23aa70b7ef62b0a89
Hrithik4579 <hbhagat2003@gmail.com>
2024-11-22 02:58:15 +0530

    final

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React from 'react'
 import NoteContext from './NoteContext'
 import { useState } from 'react'
 const NoteState=(props)=> {
-    const host="http://i-notebook-e3zg.vercel.app";
+    const host="http://localhost:5000";
     const notesInitial=[]
     const [notes,setNotes]=useState(notesInitial);
     //Fetching Notes
